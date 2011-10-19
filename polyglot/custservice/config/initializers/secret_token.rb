c40f4b54013a43c04bb1f1cc01f40c322778ca15
Thomas Risberg <trisberg@vmware.com>
2011-10-19 17:50:10 -0400

    Adding first cut of Customer Service app

---
+++ polyglot/custservice/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Custservice::Application.config.secret_token = '7a6c476cc8162f4088135e00afa65573785cb4f4f7b9e55b4b8277e0d90515f20dcd62197062436ae81d9c528784d0e1c429614dd1edcfe7fb0b5f7d863d8d64'
---
+++ polyglot/custservice/db/migrate/20111019213806_create_customers.rb
@@ -0,0 +1,20 @@
+class CreateCustomers < ActiveRecord::Migration
+  def self.up
+    create_table :customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.string :zip_code
+      t.string :creditcard_no
+      t.integer :credit_score
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :customers
+  end
+end
