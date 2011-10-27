a895f347128b8989f2fe01f93870848d9aa13569
Thomas Risberg <trisberg@vmware.com>
2011-10-27 14:10:52 -0400

    Adding customer_id

--- polyglot/custservice/config/initializers/secret_token.rb
+++ polyglot/custservice/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Custservice::Application.config.secret_token = '497270b83c830c6784e408cf15a76ef5ed7544bd4b6ef5ca891396d5aa8a8d989b5b87d28b21ff9b39ca34fa22aef27ba80337cdb4912be338be55d0139135c6'
+Custservice::Application.config.secret_token = '735a183c2dbc0ab746e768cc99a5804eefac5c17073aa81c38d7820812b40da4a9a2fab3945a7869c36c54de06186799c95c41963b40aebcb8f2592717cf8cb4'
--- polyglot/custservice/db/migrate/20111027140421_create_customers.rb
+++ polyglot/custservice/db/migrate/20111027140421_create_customers.rb
@@ -1,7 +1,6 @@
 class CreateCustomers < ActiveRecord::Migration
   def self.up
     create_table :customers do |t|
-      t.integer :id
       t.string :customer_id
       t.string :first_name
       t.string :last_name
