bd79be312a75e5b20507556231c8f4e457c77639
Thomas Risberg <trisberg@vmware.com>
2011-11-02 14:03:45 -0400

    Making the order status chart prettier

--- polyglot/monitor/src/main/java/org/wgrus/web/ChartController.java
+++ polyglot/monitor/src/main/java/org/wgrus/web/ChartController.java
@@ -1,5 +1,7 @@
 package org.wgrus.web;
 
+import java.awt.Color;
+import java.awt.GradientPaint;
 import java.io.OutputStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 @RequestMapping("/charts")
@@ -32,13 +37,34 @@ public class ChartController {
 		
 		boolean rotate = false;
 
-		return ChartFactory.createBarChart("WGRUS Order Status", // title
-				"Orders", // x-axis label
-				"Status", // y-axis label
+		JFreeChart chart = ChartFactory.createBarChart("WGRUS Order Status", // title
+				"Status", // x-axis label
+				"Count", // y-axis label
 				dataset, rotate ? PlotOrientation.HORIZONTAL
 				: PlotOrientation.VERTICAL, true, // legend displayed
 				true, // tooltips displayed
 				false); // no URLs*/
+		
+		CategoryPlot plot = chart.getCategoryPlot();
+		BarRenderer renderer = (BarRenderer) plot.getRenderer();
+		GradientPaint paintPending = new GradientPaint(
+				0.0f, 0.0f, Color.yellow, 
+				0.0f, 0.0f, new Color(0, 64, 64));
+		GradientPaint paintProcessed = new GradientPaint(
+				0.0f, 0.0f, Color.green, 
+				0.0f, 0.0f, new Color(0, 64, 0));
+		GradientPaint paintReject = new GradientPaint(
+				0.0f, 0.0f, Color.red, 
+				0.0f, 0.0f, new Color(64, 0, 0));
+		
+		renderer.setSeriesPaint(0, paintPending);
+		renderer.setSeriesPaint(1, paintProcessed);
+		renderer.setSeriesPaint(2, paintReject);
+		
+		NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        
+		return chart;
 	}
 	
 	private DefaultCategoryDataset getOrderData() {
@@ -48,9 +74,9 @@ public class ChartController {
 		long rejects = mongoTemplate.getCollection("rejects").count();
 		try {
 			ds = new DefaultCategoryDataset();
-			ds.addValue(messages, "Orders", "Pending");
-			ds.addValue(orders, "Orders", "Processed");
-			ds.addValue(rejects, "Orders", "Rejects");
+			ds.addValue(messages, "Pending", "Orders");
+			ds.addValue(orders, "Processed", "Orders");
+			ds.addValue(rejects, "Rejects", "Orders");
 			return ds;
 
 		} catch (Exception e) {
